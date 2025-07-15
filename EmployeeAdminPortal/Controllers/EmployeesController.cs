f8c4241033a30f28292350e42726b7b20f526615
shanib burney <shanib.burney@gmail.com>
2025-07-15 20:32:50 +0500

    added gitignore file

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,62 @@
+﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models.DTO;
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+         var allEmployees = _dbContext.Employees.ToList();
+
+            return Ok(allEmployees);
+
+        }
+        [HttpPost]
+        public IActionResult InsertEmployee(CreateEmployeeRequestDto request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var employee = new Employee
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name,
+                Email = request.Email,
+                Phone = request.Phone,
+                Salary = request.Salary
+            };
+
+            _dbContext.Employees.Add(employee);
+            _dbContext.SaveChanges();
+
+            return CreatedAtAction(nameof(GetEmployeeById), new { id = employee.Id }, employee);
+
+        }
+        [HttpGet("{id}")]
+        //[Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var employee = _dbContext.Employees.Find(id);
+            if (employee == null)
+                return NotFound();
+
+            return Ok(employee);
+        }
+    }
+}
---
+++ EmployeeAdminPortal/Models/DTO/EmployeeDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeAdminPortal.Models.DTO
+{
+    public class CreateEmployeeRequestDto
+    {
+        [Required, EmailAddress]
+        public required string Email { get; set; }
+
+        [Required, StringLength(100)]
+        public required string Name { get; set; }
+        public string? Phone { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal Salary { get; set; }
+    }
+}
